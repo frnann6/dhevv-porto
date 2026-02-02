00aad959629a4b0159dd02c1bfeb6bac111b0999
FernandaDwi999 <fernandadchy@gmail.com>
2026-02-02 17:00:40 +0700

    feat: add modal image navbar profile

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,18 @@
-document.querySelectorAll(".portfolio-img").forEach((img) => {
-  img.addEventListener("click", function () {
-    document.getElementById("modalImage").src = this.dataset.img;
-  });
+// portfolio images
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("portfolio-img")) {
+    document.getElementById("portfolioModalImg").src = e.target.src;
+
+    new bootstrap.Modal(document.getElementById("portfolioModal")).show();
+  }
+});
+
+// Gambar profile navbar
+const profileImg = document.getElementById("profileImg");
+
+profileImg.addEventListener("click", function () {
+  document.getElementById("profileModalImg").src = this.src;
+
+  const modal = new bootstrap.Modal(document.getElementById("profileModal"));
+  modal.show();
 });
